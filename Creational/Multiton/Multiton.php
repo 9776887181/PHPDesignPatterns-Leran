456ef5b40873887a32e8ef0990361cdb78694947
xyz <q9776887181@gmail.com>
2019-05-07 17:23:27 +0800

    multiton

---
+++ Creational/Multiton/Multiton.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DesignPatterns\Creational\Multiton;
+
+final class Multiton
+{
+    const INSTANCE_1 = '1';
+    const INSTANCE_2 = '2';
+
+
+    private static $instances = [];
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(string $instanceName): Multiton
+    {
+        if (!isset(self::$instances[$instanceName])) {
+            self::$instances[$instanceName] = new self();
+        }
+        return self::$instances[$instanceName];
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+}
